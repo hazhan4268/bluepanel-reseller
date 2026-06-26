6c06d8d71b68d224214abd5de620131fa63d3cef
-VPN <132519868+hazhan4268@users.noreply.github.com>
2026-06-26 10:55:02 +0330

    Use psycopg driver for Alembic

--- alembic/env.py
+++ alembic/env.py
@@ -8,7 +8,8 @@ from app.db.session import Base
 from app.db import models  # noqa: F401
 
 config = context.config
-config.set_main_option('sqlalchemy.url', settings.database_url.replace('+asyncpg', ''))
+sync_url = settings.database_url.replace('+asyncpg', '+psycopg')
+config.set_main_option('sqlalchemy.url', sync_url)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
